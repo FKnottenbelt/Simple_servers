ce89c50cbc675895e285e3707bdf30d2ef77c3d8
Femke Knottenbelt <femknotgithub@gmail.com>
2019-12-28 10:56:31 +0000

    Add the beginning of routes

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const routeResponseMap = {
+  '/info': '<h1>Info Page</h1>',
+  '/contact': '<h1>Contact Us</h1>',
+  '/about': '<h1>Learn more about us</h1>',
+  '/hello': '<h1>Say hello by emailing us here</h1>',
+  '/error': '<h1>Sorry the page you are looking for is not here</h1>',
+};
+
 const port = 8080;
 const http = require("http");
 const httpStatus = require("http-status-codes");
@@ -7,10 +15,11 @@ const app = http.createServer((request, response) => {
     "Content-Type": "text/html"
   });
 
-  let responseMessage = "<h1>Hello, Universe!</h1>";
-  response.write(responseMessage);
-  response.end();
-  console.log(`Sent a response: ${responseMessage}`);
+  if (routeResponseMap[request.url]) {
+    response.end(routeResponseMap[request.url]);
+  } else {
+    response.end('<h1>Welcome!</h1>');
+  }
 });
 
 app.listen(port);
