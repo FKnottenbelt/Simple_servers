69ba5ceb5625d8ca883936a83649861483b37648
Femke Knottenbelt <femknotgithub@gmail.com>
2020-01-07 18:48:15 +0000

    Add error handling

--- express_templates/main.js
+++ express_templates/main.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 app.set('port', process.env.PORT || 8080);
 const homeController = require('./controllers/homeController');
+const errorController = require('./controllers/errorController');
 app.set('view engine', 'ejs');
 const layouts = require('express-ejs-layouts');
-app.use(layouts);
 
 // middleware function
+app.use(express.static("public"));
+app.use(layouts);
+
 app.use('/', homeController.logRequestPaths);
 
 app.use(
@@ -17,10 +20,16 @@ app.use(
 
 app.use(express.json());
 
+
 // routes
 app.get('/name/:myName', homeController.respondWithName);
 
 app.get('/', homeController.homePage);
+
+// error handling
+app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
 
 // server
 app.listen(app.get('port'), () => {
